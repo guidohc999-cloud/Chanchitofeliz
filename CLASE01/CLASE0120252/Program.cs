225236251d3aef32cc24a870c269c7e5d80893e2
Guido <guidohc999@gmail.com>
2025-08-23 21:20:17 -0600

    Guardado

---
+++ CLASE01/CLASE0120252/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASE0120252
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            string nombre;
+            double sueldo;
+            edad = 19;
+            nombre = "guido";
+            sueldo = 15000;
+
+            Console.WriteLine("tu nombre es: " + nombre);
+            Console.WriteLine("tu edad es : " + edad);
+            Console.WriteLine("sueldo es : " + sueldo);
+            Console.ReadKey();
+
+            
+
+               
+
+        }
+    }
+}
